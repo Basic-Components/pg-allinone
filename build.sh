bf5bc38574e372ad71d58e5b8ebcdb2e4024f204
HUANG SIZHE <hsz1273327@gmail.com>
2022-11-01 15:12:21 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker buildx build --push --platform=linux/amd64,linux/arm64 \
--t hsz1273327/pg-allinone:pg12_0.0.4 \
--t hsz1273327/pg-allinone:pg12_latest \
+-t hsz1273327/pg-allinone:pg12-0.0.4 \
+-t hsz1273327/pg-allinone:pg12-latest \
 -f dockerfile.cn .
